cb55d38bbccc9e925a6c556931d1af61a84de869
viniciustec <vinialvesrocha@gmail.com>
2021-05-14 16:44:44 -0300

    Requisito 1 para meus amigos verem

--- src/App.js
+++ src/App.js
@@ -1,18 +1,38 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import { getCategories } from './services/api';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Categories</h1>
-      {getCategories.map(({ id, name }) => (
-        <div id={ id } key={ id }>
-          <p>{name}</p>
-        </div>
-      ))}
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      categories: [],
+    };
+  }
+
+  componentDidMount = async () => {
+    console.log(await getCategories());
+    const categories = await getCategories();
+    this.updateState(categories);
+  }
+
+  updateState = (categories) => {
+    this.setState({ categories });
+  }
+
+  render() {
+    const { categories } = this.state;
+    return (
+      <div className="App">
+        <h1>Categories</h1>
+        {categories.map(({ id, name }) => (
+          <div id={ id } key={ id }>
+            <p>{name}</p>
+          </div>
+        ))}
+      </div>
+    );
+  }
 }
 
 export default App;
