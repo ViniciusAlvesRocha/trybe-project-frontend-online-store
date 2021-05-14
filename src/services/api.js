0e3b7012f4a3df058ef80da4e58bf0ff1731b72c
viniciustec <vinialvesrocha@gmail.com>
2021-05-14 17:29:00 -0300

    Consertando erros no ESlint

--- src/services/api.js
+++ src/services/api.js
@@ -1,13 +1,11 @@
 export async function getCategories() {
   // Implemente aqui
   const response = await fetch('https://api.mercadolibre.com/sites/MLB/categories');
-  const categories = await response.json();
-  return categories;
+  return response.json();
 }
 
 export async function getProductsFromCategoryAndQuery(categoryId, query) {
   // Implemente aqui! Quando o fizer, descomente os parâmetros que essa função recebe /* * /
   const response = await fetch(`https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}&q=${query}`);
-  const productsByCategory = await response.json();
-  return productsByCategory;
+  return response.json();
 }
