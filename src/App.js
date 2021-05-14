7796b238341451a519ad68a0b3f538ad413f9e70
Gabriel Essênio <gabriel.essenio@gmail.com>
2021-05-14 18:48:01 -0300

    Fazendo requisto 4 , map das categorias

--- src/App.js
+++ src/App.js
@@ -1,35 +1,17 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
-// import NavBar from './components/NavBar';
+import Categoria from './components/Categoria';
 import Home from './components/Home';
 import './App.css';
 
 function App() {
   return (
-<<<<<<< HEAD
-    <div className="App">
-      <header className="App-header">
-        <img src={ logo } className="App-logo" alt="logo" />
-        <p>Edit src/App.js and save to reload.</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-
-          Learn React
-        </a>
-      </header>
-    </div>
-=======
     <BrowserRouter>
-      {/* <NavBar /> */}
+      <Categoria />
       <Switch>
         <Route exact path="/" component={ Home } />
       </Switch>
     </BrowserRouter>
->>>>>>> 97038ec15d1e06d20888490e4fed60dd5783a198
   );
 }
 
---
+++ src/components/Categoria.jsx
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import { NavLink } from 'react-router-dom';
+import { getCategories } from '../services/api';
+
+export default class Categoria extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      categories: [],
+    };
+  }
+
+  componentDidMount() {
+    this.getApi();
+  }
+
+getApi = async () => {
+  const categoriesApi = await getCategories();
+  this.setState({ categories: categoriesApi });
+}
+
+render() {
+  const { categories } = this.state;
+  console.log(categories);
+  return (
+    <header>
+      <nav>
+        { categories.map(({ id, name }) => (
+          <NavLink
+            data-testid="category"
+            to="/"
+            key={ id }
+          >
+            {name}
+          </NavLink>))}
+      </nav>
+    </header>
+  );
+}
+}
