1e1c073aebb4b367c126271effef63c8f9298885
viniciustec <vinialvesrocha@gmail.com>
2021-05-14 15:47:46 -0300

    Tentando buscar categorias da API

--- src/App.js
+++ src/App.js
@@ -1,22 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { getCategories } from './services/api';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={ logo } className="App-logo" alt="logo" />
-        <p>Edit src/App.js and save to reload.</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Categories</h1>
+      {getCategories.map(({ id, name }) => (
+        <div id={ id } key={ id }>
+          <p>{name}</p>
+        </div>
+      ))}
     </div>
   );
 }
--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,10 @@
 export async function getCategories() {
   // Implemente aqui
+  const response = await fetch('https://api.mercadolibre.com/sites/MLB/categories');
+  const categories = await response.json();
+  return categories;
 }
 
-export async function getProductsFromCategoryAndQuery(/* categoryId, query */) {
-  // Implemente aqui! Quando o fizer, descomente os parâmetros que essa função recebe
+export async function getProductsFromCategoryAndQuery() {
+  // Implemente aqui! Quando o fizer, descomente os parâmetros que essa função recebe /* categoryId, query* /
 }
