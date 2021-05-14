792c3bdc8765f8030457b1beded4984a1f7eec26
viniciustec <vinialvesrocha@gmail.com>
2021-05-14 17:09:27 -0300

    limpando o componente App apenas deixando o necessario

--- src/App.js
+++ src/App.js
@@ -1,38 +1,11 @@
-import React, { Component } from 'react';
+import React from 'react';
+import logo from './logo.svg';
 import './App.css';
-import { getCategories } from './services/api';
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      categories: [],
-    };
-  }
-
-  componentDidMount = async () => {
-    console.log(await getCategories());
-    const categories = await getCategories();
-    this.updateState(categories);
-  }
-
-  updateState = (categories) => {
-    this.setState({ categories });
-  }
-
-  render() {
-    const { categories } = this.state;
-    return (
-      <div className="App">
-        <h1>Categories</h1>
-        {categories.map(({ id, name }) => (
-          <div id={ id } key={ id }>
-            <p>{name}</p>
-          </div>
-        ))}
-      </div>
-    );
-  }
+function App() {
+  return (
+    <div className="App" />
+  );
 }
 
 export default App;
