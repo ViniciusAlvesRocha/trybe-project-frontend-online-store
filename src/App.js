861af044c8e8369af35971e615c5e0a7a21a243d
Gabriel Essênio <gabriel.essenio@gmail.com>
2021-05-17 16:02:26 -0300

    Tentando resolver requisito 2

--- src/App.js
+++ src/App.js
@@ -1,17 +1,15 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import Categoria from './components/Categoria';
+import NavBar from './components/NavBar';
 import Home from './components/Home';
 import './App.css';
-import EmptyCart from './components/EmptyCart';
 
 function App() {
   return (
     <BrowserRouter>
-      <Categoria />
+      <NavBar />
       <Switch>
         <Route exact path="/" component={ Home } />
-        <Route path="/emptyCart" component={ EmptyCart } />
       </Switch>
     </BrowserRouter>
   );
